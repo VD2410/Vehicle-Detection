f2bdc9c9b87c0964d6188ef73bfcbcb33feb2e6c
VD2410 <vishalbatavia88@yahoo.com>
2018-10-11 11:55:38 -0700

    Add files via upload

---
+++ polygon.py
@@ -0,0 +1,12 @@
+import json
+import numpy as np
+with open('/home/vbatvia/VC/Lab/Object detection/cityscapes_samples_labels/konigswinter/konigswinter_000000_000000_gtCoarse_polygons.json','r') as f:
+	frame_info = json.load(f)
+print(frame_info['objects'][0]['polygon'])
+
+polygons = np.asarray(frame_info['objects'][0]['polygon'],dtype = np.float32)
+left_top = np.min(polygons , axis = 0)
+right_bottom = np.max(polygons , axis = 0)
+print(left_top)
+print(right_bottom)
+
