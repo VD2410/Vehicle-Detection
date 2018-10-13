f9d4adcd70c939d344a41a0bb5ba42b797fc658b
vvignesh501 <43393145+vvignesh501@users.noreply.github.com>
2018-10-12 19:19:21 -0700

    Update cityscape_dataset.py

--- cityscape_dataset.py
+++ cityscape_dataset.py
@@ -2,23 +2,57 @@ import numpy as np
 import torch.nn
 from torch.utils.data import Dataset
 from bbox_helper import generate_prior_bboxes, match_priors
+from PIL import Image
+import matplotlib.pyplot as plt
 
 
 class CityScapeDataset(Dataset):
-
     def __init__(self, dataset_list):
         self.dataset_list = dataset_list
+        self.image_size = 300
 
         # TODO: implement prior bounding box
-        self.prior_bboxes = generate_prior_bboxes(prior_layer_cfg='Todo, use your own setting, please refer bbox_helper.py for an example')
-
+        """
+            Generate prior bounding boxes on different feature map level. This function used in 'cityscape_dataset.py'
+            Use VGG_SSD 300x300 as example:
+            Feature map dimension for each output layers:
+               Layer     | Map Dim (h, w)  | Single bbox size that covers in the original image
+            1. Conv6     | (38x38)         | (30x30) (unit. pixels)
+            1. Conv11    | (19x19)        | (60x60) (unit. pixels)
+            2. Conv13    | (10x10)        | (113x113)
+            3. Conv14_2  | (5x5)          | (165x165)
+            4. Conv15_2  | (3x3)          | (218x218)
+            5. Conv16_2  | (1x1)          | (270x270)
+            6. Conv17_2  | (1x1)          | (264x264)
+            NOTE: The setting may be different using MobileNet v3, you have to set your own implementation.
+            Tip: see the reference: 'Choosing scales and aspect ratios for default boxes' in original paper page 5.
+            :param prior_layer_cfg: configuration for each feature layer, see the 'example_prior_layer_cfg' in the following.
+            :return prior bounding boxes with form of (cx, cy, w, h), where the value range are from 0 to 1, dim (1, num_priors, 4)
+            """
+        prior_layer_cfg = [{'layer_name': 'Conv6', 'feature_dim_hw': (38, 38), 'bbox_size': (30, 30),
+                            'aspect_ratio': (1.0, 1 / 2, 1 / 3, 2.0, 3.0)},
+                           {'layer_name': 'Conv11', 'feature_dim_hw': (19, 19), 'bbox_size': (60, 60),
+                            'aspect_ratio': (1.0, 1 / 2, 1 / 3, 2.0, 3.0)},
+                           {'layer_name': 'Conv13', 'feature_dim_hw': (10, 10), 'bbox_size': (102, 102),
+                            'aspect_ratio': (1.0, 1 / 2, 1 / 3, 2.0, 3.0)},
+                           {'layer_name': 'Conv14_2', 'feature_dim_hw': (5, 5), 'bbox_size': (144, 144),
+                            'aspect_ratio': (1.0, 1 / 2, 1 / 3, 2.0, 3.0)},
+                           {'layer_name': 'Conv15_2', 'feature_dim_hw': (3, 3), 'bbox_size': (186, 186),
+                            'aspect_ratio': (1.0, 1 / 2, 1 / 3, 2.0, 3.0)},
+                           {'layer_name': 'Conv16_2', 'feature_dim_hw': (2, 2), 'bbox_size': (228, 228),
+                            'aspect_ratio': (1.0, 1 / 2, 1 / 3, 2.0, 3.0)},
+                           {'layer_name': 'Conv17_2', 'feature_dim_hw': (1, 1), 'bbox_size': (270, 270),
+                            'aspect_ratio': (1.0, 1 / 2, 1 / 3, 2.0, 3.0)}
+                           ]
+        self.prior_bound_boxes = generate_prior_bboxes(prior_layer_cfg)
+        print(dataset_list)
         # Pre-process parameters:
         #  Normalize: (I-self.mean)/self.std
         self.mean = np.asarray((127, 127, 127))
         self.std = 128.0
 
     def get_prior_bbox(self):
-        return self.prior_bboxes
+        return self.prior_bound_boxes
 
     def __len__(self):
         return len(self.dataset_list)
@@ -36,21 +70,41 @@ class CityScapeDataset(Dataset):
         # 3. Convert the bounding box from corner form (left-top, right-bottom): [(x,y), (x+w, y+h)] to
         #    center form: [(center_x, center_y, w, h)]
         # 4. Normalize the bounding box position value from 0 to 1
-        sample_labels = None
-        sample_bboxes = None
-        sample_img = None
+        item = self.dataset_list[idx]
+        self.image_path = item['path']
+        self.labels = np.asarray(item['labels'])
+        self.labels = torch.Tensor(self.labels)
+        self.bound_boxes = item['bound_boxes']
+        self.bound_boxes = torch.Tensor(np.asarray(item['bound_boxes'], dtype=np.float32))
+        print(self.bound_boxes.shape)
+
+        img = Image.open(self.image_path)
+
+        w, h = img.size
+        self.bound_boxes /= torch.Tensor([w, h, w, h]).expand_as(self.bound_boxes)
+
+        # resize image
+        img = img.resize((self.image_size, self.image_size), Image.ANTIALIAS)
+        # normalize_img
+        img = np.asarray(img, dtype=np.float32)
+        # normalise the image pixels to (-1,1)
+        img = (img / 255.0) * 2 - 1
+
+        # convert to tensor
+        img_tensor = torch.Tensor(img.astype(float))
+        img_tensor = img_tensor.view((img.shape[2], img.shape[0], img.shape[1]))
 
         # 4. Do the augmentation if needed. e.g. random clip the bounding box or flip the bounding box
 
         # 5. Do the matching prior and generate ground-truth labels as well as the boxes
-        bbox_tensor, bbox_label_tensor = match_priors(self.prior_bboxes, sample_bboxes, sample_labels, iou_threshold=0.5)
-
+        bbox_tensor, bbox_label_tensor = match_priors(self.prior_bound_boxes, self.bound_boxes, self.labels,
+                                                      iou_threshold=0.5)
         # [DEBUG] check the output.
-        assert isinstance(bbox_label_tensor, torch.Tensor)
-        assert isinstance(bbox_tensor, torch.Tensor)
-        assert bbox_tensor.dim() == 2
-        assert bbox_tensor.shape[1] == 4
-        assert bbox_label_tensor.dim() == 1
-        assert bbox_label_tensor.shape[0] == bbox_tensor.shape[0]
+        # assert isinstance(bbox_label_tensor, torch.Tensor)
+        # assert isinstance(bbox_tensor, torch.Tensor)
+        # assert bbox_tensor.dim() == 2
+        # assert bbox_tensor.shape[1] == 4
+        # assert bbox_label_tensor.dim() == 1
+        # assert bbox_label_tensor.shape[0] == bbox_tensor.shape[0]
 
-        return bbox_tensor, bbox_label_tensor
+        return img_tensor, bbox_tensor, bbox_label_tensor
